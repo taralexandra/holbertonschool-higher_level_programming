4c0768b33cf3531248951bd1a8c3707b27cc3e59
taralexandra <taralexandra@gmail.com>
2024-09-09 11:46:37 +0200

    I am adding file for task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for number in row:
+            print("{:d}".format(number), end="")
+            if number != row[-1]:
+                print(" ", end="")
+        print()
