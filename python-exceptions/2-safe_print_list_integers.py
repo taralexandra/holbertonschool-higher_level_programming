d0368389d906df8dd6b5b24392af14b1402ba525
taralexandra <taralexandra@gmail.com>
2024-09-11 15:44:12 +0200

    updating file 2, tsk 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
             count += 1
         except (ValueError, TypeError):
-            pass
-        except IndexError:
-            break
+            continue
     print("")
     return count
