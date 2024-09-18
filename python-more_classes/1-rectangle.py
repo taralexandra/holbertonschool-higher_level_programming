b347fc791021d21e92e16504b7e726a0f6629f2a
taralexandra <taralexandra@gmail.com>
2024-09-18 15:05:38 +0200

    i added an underscore and this is looking better, lets give it a try

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
