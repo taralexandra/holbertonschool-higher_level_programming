e997bc58589cc21c0ff2aba1b494fcf4a166f7f1
taralexandra <taralexandra@gmail.com>
2024-09-18 10:26:38 +0200

    adding file 0 for task 0 empty class Rectangle and readme file

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+I am making a file to build the rectangle class
+which will allow to print a rectangle at the
+end of the project.
+"""
+
+
+class Rectangle:
+    """
+        This is the empty class of the rectangle
+    """
+
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,4 @@
+# This README file is linked to the project Python - More Classes & Objects :ledger:
+Feel free to read it and enjoy.
+September 2024.
+
