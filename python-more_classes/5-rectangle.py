f215d15239bbf98d5cf0a674fdf5e7b3e08ca9e7
taralexandra <taralexandra@gmail.com>
2024-09-19 15:53:36 +0200

    i add file 5, for task 5, wich will print a specific msg when instance is deleted

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+"""
+    I am making a collection of files to
+    build the rectangle class,
+    in order to print a rectangle at the
+    end of the project.
+"""
+
+
+class Rectangle:
+
+    """
+        This is the class of the rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+
+        """
+            Let's make a rectangle.
+
+            Args:
+
+                width (int)
+                height (int)
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        "property to give the width of the rectangle's shape"
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        objective is to get the value of width
+
+        Args:
+             value
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        objective here is to get value of height
+
+        Args:
+            value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Will return the area of the rectangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Will return the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return ((self.width + self.height) * 2)
+
+    def __str__(self):
+        """Return the rectangle made as a string made of symbols #"""
+        if self.width == 0 or self.height == 0:
+            return ""
+
+        rect_lines = []
+        for _ in range(self.height):
+            rect_lines.append("#" * self.width)
+
+        return "\n".join(rect_lines)
+
+    def __repr__(self):
+        """Return string representation to create a new instance"""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """Print a specific msg when the instance is deleted"""
+        print("Bye rectangle...")
