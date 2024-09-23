82a60e7d703d6188a8f1c9d6a32371e05dd0c173
taralexandra <taralexandra@gmail.com>
2024-09-23 16:08:27 +0200

    i add the file 0, task 0, look up, retrn list of attributes of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    adding a comment for this task
+"""
+
+
+def lookup(obj):
+    """
+    Commentating this function,
+    Will return list of attributes
+    and objects available
+    """
+    return dir(obj)
