7f87e804a8b4a6ddfef67073f121c0dd8b1bb06d
taralexandra <taralexandra@gmail.com>
2024-09-24 10:36:17 +0200

    i add the file 4 for task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    Write a function that returns True if the object
+    is an instance of a class that inherited
+    (directly or indirectly) from the
+    specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """the function"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
